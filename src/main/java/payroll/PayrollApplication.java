2102a45ee7ac95f48fd2c38bfde667d56ebf181f
koreanwizard <harry992178@gmail.com>
2025-02-17 15:35:58 +0900

    last commit for the nonrest

--- src/main/java/payroll/PayrollApplication.java
+++ src/main/java/payroll/PayrollApplication.java
@@ -12,16 +12,4 @@ public class PayrollApplication {
 
 }
 
-// first commit from my Windows desktop
-//package payroll;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class PayrollApplication {
-//
-//	public static void main(String... args) {
-//		SpringApplication.run(PayrollApplication.class, args);
-//	}
-//}
+// A last commit for the nonrest. At this moment, this project will contain RESTful service.
