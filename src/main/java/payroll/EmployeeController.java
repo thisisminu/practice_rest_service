4331ccffff678bdce86b565bbb3c1b3fb443a7e0
koreanwizard <harry992178@gmail.com>
2025-02-17 19:14:06 +0900

    studied how to use HATEOAS to create RESTful service

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -1,7 +1,9 @@
 package payroll;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,11 +27,27 @@ class EmployeeController {
 
     // Aggregate root
     // tag::get-aggregate-root[]
+//    @GetMapping("/employees")
+//    List<Employee> all() {
+//        return repository.findAll();
+//    }
+//    // end::get-aggregate-root[]
+
+
+    // Every item  (RESTful version)
     @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
+    CollectionModel<EntityModel<Employee>> all() {
+
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(employee -> EntityModel.of(employee,
+                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
-    // end::get-aggregate-root[]
+
+
 
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
